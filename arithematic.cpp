db1fdf6a418f6208e251f4c23599b4983981e5b8
ayushksingh-19 <singhayushkk@gmail.com>
2026-04-30 18:52:54 +0530

    arithemetic operators

---
+++ arithematic.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    double a, b;
+    char op;
+
+    cout << "Enter first number: ";
+    cin >> a;
+
+    cout << "Enter operator (+, -, *, /): ";
+    cin >> op;
+
+    cout << "Enter second number: ";
+    cin >> b;
+
+    switch(op) {
+        case '+':
+            cout << "Result: " << a + b;
+            break;
+
+        case '-':
+            cout << "Result: " << a - b;
+            break;
+
+        case '*':
+            cout << "Result: " << a * b;
+            break;
+
+        case '/':
+            cout << "Result: " << a / b;
+            break;
+
+        default:
+            cout << "Invalid operator";
+    }
+
+    return 0;
+}
