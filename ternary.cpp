c3e794e9cfda309e6bebb149c0b293d47ea03877
ayushksingh-19 <singhayushkk@gmail.com>
2026-05-01 10:12:09 +0530

    use of ternary conditional op

---
+++ ternary.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    int maxNum = (a > b) ? a : b;
+    cout << "Maximum number is " << maxNum;
+
+    return 0;
+}
