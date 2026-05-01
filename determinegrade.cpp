a2ecff8b405919bdc521f3241a778e24629a03f3
ayushksingh-19 <singhayushkk@gmail.com>
2026-05-01 11:17:14 +0530

    determine grade from percentage

---
+++ determinegrade.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float percentage;
+
+    cout << "Enter student percentage (0 to 100): ";
+    cin >> percentage;
+
+    if (percentage < 0 || percentage > 100) {
+        cout << "Invalid input" << endl;
+    } else if (percentage >= 90) {
+        cout << "Grade A" << endl;
+    } else if (percentage >= 80) {
+        cout << "Grade B" << endl;
+    } else if (percentage >= 70) {
+        cout << "Grade C" << endl;
+    } else if (percentage >= 60) {
+        cout << "Grade D" << endl;
+    } else {
+        cout << "Fail" << endl;
+    }
+
+    return 0;
+}
