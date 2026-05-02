ec380efdac60194e3a43ed6c841b0f111494ee40
ayushksingh-19 <singhayushkk@gmail.com>
2026-05-02 10:13:57 +0530

    use for loop to check divisibility by 2

---
+++ day 3/divisibleby2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter an integer: ";
+    cin >> n;
+
+    cout << "Numbers between 1 and " << n << " divisible by 2 are:" << endl;
+
+    for (int i = 1; i <= n; i++) {
+        if (i % 2 == 0) {
+            cout << i << " ";
+        }
+    }
+
+    return 0;
+}
