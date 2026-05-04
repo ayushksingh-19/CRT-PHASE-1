82e63e4c0f69dde5ce972617224ecfc74e7548e7
ayushksingh-19 <singhayushkk@gmail.com>
2026-05-04 11:26:33 +0530

    hollow square

---
+++ day 4/hollowsquare.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i, j, rows;
+
+    cout << "Enter number of rows: ";
+    cin >> rows;
+
+    for (i = 1; i <= rows; i++) {
+        for (j = 1; j <= rows; j++) {
+            if (i == 1 || i == rows || j == 1 || j == rows) {
+                cout << "*";
+            } else {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
