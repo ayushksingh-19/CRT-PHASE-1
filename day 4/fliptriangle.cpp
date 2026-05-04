00815dd4a6b957db6c28b697eed05770c8d1bf76
ayushksingh-19 <singhayushkk@gmail.com>
2026-05-04 10:26:01 +0530

    triangle pattern

---
+++ day 4/fliptriangle.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i, j, rows;
+
+    cout << "Enter number of rows: ";
+    cin >> rows;
+
+    for (i = 1; i <= rows; i++) {
+        for (j = 1; j <= rows - i; j++) {
+            cout << " ";
+        }
+        for (j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
