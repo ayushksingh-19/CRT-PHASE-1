7d316f9e12dd018e2beb01780718fe142c1d4c8b
ayushksingh-19 <singhayushkk@gmail.com>
2026-05-05 09:41:18 +0530

    without argument without return type

---
+++ day 5/withoutargwithoutreturntype.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void sum() {
+    int a, b;
+    cin >> a >> b;
+    cout << "Sum = " << a + b << endl;
+}
+
+int main() {
+    sum();
+    return 0;
+}
