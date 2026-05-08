4e3d775d98227646451a1a5a274cd5ce30b7873e
ayushksingh-19 <singhayushkk@gmail.com>
2026-05-08 09:42:26 +0530

    data abstraction

---
+++ day 8/abstraction.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+class ATM{
+    private:
+    int balance;
+    public:
+    ATM(int balance){
+        this->balance=balance;
+    }
+    void withdarwa(int amount){
+        if(amount>balance){
+            cout<<"insufficient balance"<<endl;
+        }
+        else{
+        balance-=amount;
+        cout<<"Amount withdrawn:"<<amount<<endl;
+        }
+        cout<<"current balance:"<<balance<<endl;
+    }
+};
+int main(){
+    ATM a(50000);
+    
+    a.withdarwa(500);
+    return 0;
+
+}
