42591116d14bca6cbf5b676955bc70d2e0648909
ayushksingh-19 <singhayushkk@gmail.com>
2026-05-11 11:15:41 +0530

    binary search leetcode question

---
+++ day 11/binarysearchleet.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int start = 0;
+        int end = nums.size() - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+};
+
+int main() {
+    Solution obj;
+    vector<int> nums = {-1, 0, 3, 5, 9, 12};
+    int target = 9;
+
+    int result = obj.search(nums, target);
+
+    if (result == -1) {
+        cout << "Element not found" << endl;
+    } else {
+        cout << "Element found at index " << result << endl;
+    }
+
+    return 0;
+}
