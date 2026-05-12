45b09fc12ef98d6d3f21c2bfd74bcabce127d0f7
ayushksingh-19 <singhayushkk@gmail.com>
2026-05-12 11:52:23 +0530

    print sum of digits using recursion

---
+++ day 12/sumofdigits.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int sumofdigit(int n)
+{
+    if(n==0)
+    return 0;
+    return(n%10 + sumofdigit(n/10));
+}
+int main()
+{
+    int num=12345;
+    int result=sumofdigit(num);
+    cout<<result<<endl;
+    return 0;
+}
